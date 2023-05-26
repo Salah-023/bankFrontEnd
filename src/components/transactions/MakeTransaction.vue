41e87718b300ad0055b9f26036dd35bd02eabfcf
AndreiSchereper <116653548+AndreiSchereper@users.noreply.github.com>
2023-05-26 11:32:57 +0200

    transactions for the employee only

---
+++ src/components/transactions/MakeTransaction.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+            <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Make a Transaction</h2>
+        </div>
+        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form class="space-y-6" action="#" method="POST">
+                <div>
+                    <label for="senderIban" class="block text-sm font-medium leading-6 text-gray-900">Iban of Sender</label>
+                    <div class="mt-2">
+                        <input id="senderIban" name="senderIban" type="text" autocomplete="senderIban" required=""
+                            placeholder="   NL00 BANK 0000 0000 00"
+                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-500 sm:text-sm sm:leading-6" />
+                    </div>
+                </div>
+                
+                <div>
+                    <label for="receiverIban" class="block text-sm font-medium leading-6 text-gray-900">Iban of Receiver</label>
+                    <div class="mt-2">
+                        <input id="receiverIban" name="receiverIban" type="text" autocomplete="receiverIban" required=""
+                            placeholder="   NL00 BANK 0000 0000 00"
+                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-500 sm:text-sm sm:leading-6" />
+                    </div>
+                </div>
+
+                <div>
+                    <label for="amount" class="block text-sm font-medium leading-6 text-gray-900">Amount</label>
+                    <div class="mt-2">
+                        <input id="amount" name="amount" type="number" autocomplete="amount" required=""
+                            placeholder="  45.50"
+                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-500 sm:text-sm sm:leading-6" />
+                    </div>
+                </div>
+
+                <div>
+                    <button type="submit"
+                        class="flex w-full justify-center rounded-md bg-teal-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-teal-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-500">
+                        Make Transaction</button>
+                </div>
+            </form>
+
+             <router-link to="/transactionList" class="font-semibold leading-6 text-teal-600 hover:text-teal-500"
+               active-class="active">Click here to see all transactions</router-link>
+    </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'MakeTransactionView'
+}
+</script>
+
+<style scoped></style>
---
+++ src/components/transactions/TransactionList.vue
@@ -0,0 +1,47 @@
+<template>
+    <main>
+        <div class="container mx-auto py-8">
+            <h2 class="text-2xl font-bold mb-4">Inholland Bank Transaction List</h2>
+            <div class="overflow-x-auto">
+                <table class="min-w-full bg-white border border-gray-300">
+                    <thead>
+                        <tr>
+                            <th class="py-2 px-4 border-b">Iban of Sender</th>
+                            <th class="py-2 px-4 border-b">Iban of Receiver</th>
+                            <th class="py-2 px-4 border-b">Amount</th>
+                            <th class="py-2 px-4 border-b">Date of Transaction</th>
+                            <th class="py-2 px-4 border-b"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="transaction in transactions">
+                            <td class="py-2 px-4 border-b">{{ transaction.senderIban }}</td>
+                            <td class="py-2 px-4 border-b">{{ transaction.receiverIban }}</td>
+                            <td class="py-2 px-4 border-b">{{ transaction.amount }}</td>
+                            <td class="py-2 px-4 border-b">{{ transaction.date }}</td>
+                            <td class="py-2 px-4 border-b">
+                                <button class="text-blue-500 hover:text-blue-700">Make Transaction</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+    name: "TransactionList",
+    data() {
+        return {
+            transactions: [
+                { senderIban: "NL00 BANK 0000 0000 01", receiverIban: "NL00 BANK 0000 0000 02", amount: "60.75", date: "2023-05-20T10:30:20" },
+                { senderIban: "NL00 BANK 0000 0000 02", receiverIban: "NL00 BANK 0000 0000 03", amount: "300", date: "2023-05-20T10:35:13" },
+                { senderIban: "NL00 BANK 0000 0000 03", receiverIban: "NL00 BANK 0000 0000 04", amount: "202.34", date: "2023-05-20T10:44:47" }
+            ]
+        };
+    },
+
+};
+</script>
