66f049bfa315d06decbe1ea96bf8f8b73a130323
Beril (Beth) <90333992+bberildundar@users.noreply.github.com>
2023-05-28 12:57:35 +0200

    fixed some things in the views

---
+++ src/views/customerViews/EditProfileView.vue
@@ -0,0 +1,77 @@
+<template>
+    <main>
+        <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+            <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+                <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Edit Profile Details
+                </h2>
+            </div>
+
+            <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form class="space-y-6" action="#" method="POST">
+                    <div>
+                        <label for="firstName" class="block text-sm font-medium leading-6 text-gray-900">First Name</label>
+                        <div class="mt-2">
+                            <input id="firstName" name="firstName" type="text" autocomplete="firstName" required=""
+                                class="pl-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-500 sm:text-sm sm:leading-6" />
+                        </div>
+                    </div>
+
+                    <div>
+                        <label for="lastName" class="block text-sm font-medium leading-6 text-gray-900">Last Name</label>
+                        <div class="mt-2">
+                            <input id="lastName" name="lastName" type="text" autocomplete="lastName" required=""
+                                class="pl-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-500 sm:text-sm sm:leading-6" />
+                        </div>
+                    </div>
+
+                    <div>
+                        <label for="phoneNumber" class="block text-sm font-medium leading-6 text-gray-900">Phone
+                            Number</label>
+                        <div class="mt-2">
+                            <input id="phoneNumber" name="phoneNumber" type="tel" autocomplete="tel" required=""
+                                class="pl-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-500 sm:text-sm sm:leading-6" />
+                        </div>
+                    </div>
+
+                    <div>
+                        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+                        <div class="mt-2">
+                            <input id="email" name="email" type="email" autocomplete="email" required=""
+                                class="pl-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-500 sm:text-sm sm:leading-6" />
+                        </div>
+                    </div>
+
+                    <div>
+                        <label for="password" class="block text-sm font-medium leading-6 text-gray-900">New Password</label>
+                        <div class="mt-2">
+                            <input id="password" name="password" type="password" autocomplete="new-password" required=""
+                                class="pl-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-500 sm:text-sm sm:leading-6" />
+                        </div>
+                    </div>
+
+                    <div>
+                        <label for="passwordAgain" class="block text-sm font-medium leading-6 text-gray-900">Confirm New
+                            Password</label>
+                        <div class="mt-2">
+                            <input id="passwordAgain" name="passwordAgain" type="password"
+                                autocomplete="new-passwordAgain" required=""
+                                class="pl-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-500 sm:text-sm sm:leading-6" />
+                        </div>
+                    </div>
+
+                    <div style="display: flex; justify-content: center;">
+                        <button type="submit"
+                            class="rounded-md bg-teal-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-teal-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-500">
+                            Save Changes
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </main>
+</template>
+  
+<script>
+
+</script>
+  
--- src/views/customerViews/customerDashboardView.vue
+++ src/views/customerViews/customerDashboardView.vue
@@ -1,18 +1,16 @@
 <template>
     <main>
-        <div class="container mx-auto py-8">
+        <div class="h-screen container mx-auto py-8">
             <div class="flex items-center justify-between">
-                <p class="text-4xl font-bold mx-5 mt-5">Welcome to Inholland Bank, {{ user.firstName }} {{ user.lastName }}
-                </p>
-                <button class="bg-white hover:bg-teal-500 text-teal-700 text-teal font-bold py-2 px-4 rounded" style="box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
-  Edit Profile
-</button>
-
+                <p class="text-4xl font-bold mx-5" style="margin-top: 7rem;">Welcome to Inholland Bank, {{ user.firstName }} {{ user.lastName }}</p>
+                <button class="bg-white hover:bg-teal-500 text-teal-700 text-teal font-bold py-2 px-4 rounded"
+                    style="box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
+                    Edit Profile
+                </button>
             </div>
-
             <div class="grid grid-cols-3 gap-4 my-5">
                 <div class="col-span-2">
-                    <div class="rounded-lg bg-teal-200 p-4"
+                    <div class="rounded-lg bg-teal-200 p-4 "
                         style="background-color: #b9eae4; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
                         <p class="text-2xl font-bold" style="color: #333;">My Accounts:</p>
 
@@ -34,27 +32,21 @@
                 </div>
                 <div class="col-span-1">
                     <button
-                        class="w-full h-50 block bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded my-2"
+                        class="w-full h-50 block bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded mb-2"
                         style="background-color: #109385;">
                         <img src="/transfer-icon.png" alt="Image" class="inline-block h-10 w-10 mr-5">
                         MAKE A TRANSACTION
                     </button>
 
                     <button
-                        class="w-full h-50 block bg-white hover:bg-teal-500 text-teal-500 font-bold py-2 px-4 rounded my-2"
+                        class="w-full h-50 block bg-white hover:bg-teal-500 text-teal-500 font-bold py-2 px-4 rounded mb-2"
                         style="box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);"> <img src="/search-icon.png" alt="Image"
                             class="inline-block h-9 w-7 mr-5">
                         SEARCH FOR AN IBAN
                     </button>
                 </div>
-
-
-
             </div>
-
         </div>
-
-
     </main>
 </template>
   
