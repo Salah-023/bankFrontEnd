8d9156269b6389f44638354c77f1d261bcc338f5
Beril (Beth) <90333992+bberildundar@users.noreply.github.com>
2023-06-02 13:27:00 +0200

    login view entry field padding

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -12,7 +12,7 @@
 
             <input id="email" name="email" type="email" autocomplete="email" required=""
               placeholder="example@email.com"
-              class="pl-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-500 sm:text-sm sm:leading-6"
+              class="px-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-500 sm:text-sm sm:leading-6"
               v-model="username" />
           </div>
         </div>
@@ -23,7 +23,7 @@
           <div class="mt-2">
             <input id="password" name="password" type="password" autocomplete="current-password" required=""
               placeholder="***********"
-              class="pl-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-500 sm:text-sm sm:leading-6"
+              class="px-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-500 sm:text-sm sm:leading-6"
               v-model="password" />
           </div>
         </div>
