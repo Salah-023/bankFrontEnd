81deb7deaaa5de90454aeb337a41fccf2061aa0e
Beril (Beth) <90333992+bberildundar@users.noreply.github.com>
2023-06-08 11:34:19 +0200

    customer's make transaction UI

--- src/components/transactions/MakeTransaction.vue
+++ src/components/transactions/MakeTransaction.vue
@@ -1,51 +1,80 @@
 <template>
-    <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-            <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Make a Transaction</h2>
+    <div class="h-700 container mx-auto py-8">
+        <div class="mb-5" id="transaction-header">
+            <h1 class="mt-5 mb-5 text-center text-4xl font-bold leading-9 tracking-tight text-gray-900">Make a Transaction
+            </h1>
         </div>
-        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                <label v-if="this.store.getRoles.includes('ROLE_CUSTOMER')" class="flex items-center">
-                    <input type="checkbox" v-model="filterForSavingsAccount">
-                    <span class="ml-2">Use your savings account</span>
-                </label>
-            <form ref="form" class="space-y-6">
-                <div v-if="this.store.getRoles.includes('ROLE_EMPLOYEE')">
-                    <label  for="accountFrom" class="block text-sm font-medium leading-6 text-gray-900">Iban of Sender</label>
-                    <div class="mt-2">
-                        <input id="accountFrom" name="accountFrom" type="text" autocomplete="accountFrom" required=""
-                            placeholder="   NL00 BANK 0000 0000 00"
-                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-500 sm:text-sm sm:leading-6" 
-                            v-model="transaction.accountFrom.iban"/>
-                    </div>
-                </div>          
-                <div v-if="!filterForSavingsAccount">
-                    <label for="accountTo" class="block text-sm font-medium leading-6 text-gray-900">Iban of Receiver</label>
-                    <div class="mt-2">
-                        <input id="accountTo" name="accountTo" type="text" autocomplete="accountTo" required=""
-                            placeholder="   NL00 BANK 0000 0000 00"
-                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-500 sm:text-sm sm:leading-6" 
-                            v-model="accountTo"/>
-                    </div>
-                </div>
 
-                <div>
-                    <label for="amount" class="block text-sm font-medium leading-6 text-gray-900">Amount</label>
-                    <div class="mt-2">
-                        <input id="amount" name="amount" type="number" autocomplete="amount" required=""
-                            placeholder="  45.50"
-                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-500 sm:text-sm sm:leading-6" 
-                            v-model="transaction.amount"/>
-                    </div>
+        <div class="mt-5 grid grid-cols-5 gap-3" id="transaction-options">
+            <div class="flex items-center pl-4 border border-teal-700 rounded" style="background-color: white;">
+                <input checked id="savingToCurrent" type="radio" value="1" name="bordered-radio"
+                    class="w-4 h-4 text-teal-600 bg-gray-100 border-teal-700 focus:ring-blue-500"
+                    style="border-color: #14b8a6;">
+                <label for="savingToCurrent" class="w-full py-4 ml-2 text-sm font-bold text-teal-600 ">From My
+                    Savings to My Current</label>
+            </div>
+            <div class="flex items-center pl-4 border border-gray-200 rounded" style="background-color: white;">
+                <input id="currentToSaving" type="radio" value="2" name="bordered-radio"
+                    class="w-4 h-4 text-teal-600 bg-gray-100 border-gray-300 focus:ring-blue-500"
+                    style="border-color: #14b8a6;">
+                <label for="currentToSaving" class="w-full py-4 ml-2 text-sm font-bold text-teal-600">From My
+                    Current To My Savings</label>
+            </div>
+            <div class="flex flex-col pl-4 border border-gray-200 rounded" style="background-color: white;">
+                <div class="mt-3 flex items-center">
+                    <input id="currentToCurrent" type="radio" value="3" name="bordered-radio"
+                        class="w-4 h-4 text-teal-600 bg-gray-100 border-gray-300 focus:ring-blue-500"
+                        style="border-color: #14b8a6;">
+                    <label for="currentToCurrent" class="ml-2 text-sm font-bold text-teal-600">From My Current to
+                        Other Current</label>
                 </div>
-
-                <div>
-                    <button type="button" @click="makeTransaction()"
-                        class="flex w-full justify-center rounded-md bg-teal-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-teal-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-500">
-                        Make Transaction</button>
+                <div class="flex items-center mt-2 mb-3">
+                    <span class="text-teal-600 font-bold">IBAN:</span>
+                    <input id="otherIban" type="text" placeholder="NL00INHO01234567800"
+                        class="pl-2 ml-2 bg-white border-teal-300 border focus:outline-none focus:ring-2 focus:ring-blue-500 w-48" />
                 </div>
-            </form>
+            </div>
+            <div class="flex items-center pl-4 border border-gray-200 rounded" style="background-color: white;">
+                <input id="deposit" type="radio" value="4" name="bordered-radio"
+                    class="w-4 h-4 text-teal-600 bg-gray-100 border-gray-300 focus:ring-blue-500"
+                    style="border-color: #14b8a6;">
+                <label for="deposit" class="w-full py-4 ml-2 text-sm font-bold text-teal-600">Deposit
+                    Money</label>
+            </div>
+            <div class="flex items-center pl-4 border border-gray-200 rounded" style="background-color: white">
+                <input id="withdraw" type="radio" value="5" name="bordered-radio"
+                    class="w-4 h-4 text-teal-600 bg-gray-100 border-gray-300 focus:ring-blue-500"
+                    style="border-color: #14b8a6;">
+                <label for="withdraw" class="w-full py-4 ml-2 text-sm font-bold text-teal-600 ">Withdraw
+                    Money</label>
+            </div>
+        </div>
+        <div class="flex justify-center items-center ml-2 mt-5 mb-3">
+            <span class="text-black text-3xl font-bold">Amount:</span>
+            <input id="amount" type="text" placeholder="100.00"
+                class="pl-2 ml-4 bg-white border-gray-300 border focus:outline-none focus:ring-2 focus:ring-blue-500 w-24 h-8 rounded"/>
+            <span class="text-black font-bold text-xl ml-3">€</span>
 
         </div>
+
+        <div style="display: flex; justify-content: center;">
+            <router-link to="/customerDashboard"
+                class="mx-2 mt-4 rounded-md bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4">
+                Cancel
+            </router-link>
+
+            <button type="button"
+                class="mx-2 mt-4 rounded-md bg-emerald-500 hover:bg-emerald-600 text-white font-bold py-2 px-4">
+                Confirm
+            </button>
+
+        </div>
+
+
+
+
+
+
     </div>
 </template>
 <script>
@@ -70,14 +99,14 @@ export default {
             },
             bankAccounts: [],
             currentAccount: {
-                iban:""
+                iban: ""
             },
             savingsAccount: {
-                iban:""
+                iban: ""
             },
             store: userStore(),
-            filterForSavingsAccount:false
-            };
+            filterForSavingsAccount: false
+        };
     },
     mounted() {
         const token = localStorage.getItem('token');
@@ -110,14 +139,14 @@ export default {
                     this.transaction.accountTo = this.currentAccount;
                 } else {
                     this.transaction.accountFrom = this.currentAccount;
-                    this.transaction.accountTo.iban=this.accountTo;
-                }                               
+                    this.transaction.accountTo.iban = this.accountTo;
+                }
             }
             axios
                 .post("/transactions", this.transaction, {
                     headers: {
-                    Authorization: `Bearer ${this.store.getToken}`
-                }
+                        Authorization: `Bearer ${this.store.getToken}`
+                    }
                 })
                 .then((res) => {
                     console.log(res.data);
