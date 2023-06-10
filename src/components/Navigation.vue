70512c35354b19f9f5ec70b49fcf19ec34c3ea44
Ign6779 <ignacio.deeusebio.cobo@gmail.com>
2023-06-10 15:36:08 +0200

    added more stuff to the navbar

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -16,14 +16,42 @@
       <div class="text-l lg:flex-grow">
         <router-link to="/" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
           active-class="active">Home</router-link>
+
+        <router-link :to="{ path: isLoggedIn ? '/customerDashboard' : '#' }"
+        class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+        :class="{ 'text-gray-400 cursor-not-allowed': !isLoggedIn }"
+        active-class="active" :disabled="!isLoggedIn" v-tooltip="{ content: 'Must be logged in' }">Your dashboard</router-link>
+
+        <router-link :to="{ path: isLoggedIn ? '/makeTransaction' : '#' }"
+          class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
+          :class="{ 'text-gray-400 cursor-not-allowed': !isLoggedIn }"
+          active-class="active" :disabled="!isLoggedIn" v-tooltip="{ content: 'Must be logged in' }">Make transaction</router-link>
+
+        <router-link v-if="this.store.getRoles.includes('ROLE_EMPLOYEE')" to="/bankAccountList"
+        class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
+        active-class="active">Bank account list</router-link>
+
+      <router-link v-if="this.store.getRoles.includes('ROLE_EMPLOYEE')" to="/users"
+        class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
+        active-class="active">User list </router-link>
+
+      <router-link v-if="this.store.getRoles.includes('ROLE_EMPLOYEE')" to="/employeeDashboard"
+        class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
+        active-class="active">Employee dashboard</router-link>
+
+      <router-link v-if="this.store.getRoles.includes('ROLE_EMPLOYEE')" to="/employeeMakeTransaction"
+        class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
+        active-class="active">Employee transaction</router-link>
       </div>
       <div>
         <button v-if="isLoggedIn" type="button" @click="logout"
           class="inline-block text-l px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 mt-4 lg:mt-0"
-          active-class="active">Logout</button>
+          active-class="active">Logout
+        </button>
         <router-link v-else to="/login"
           class="inline-block text-l px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 mt-4 lg:mt-0"
-          active-class="active">Log in</router-link>
+          active-class="active">Log in
+        </router-link>
       </div>
     </div>
   </nav>
